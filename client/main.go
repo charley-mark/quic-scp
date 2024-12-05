29f791578ed3524d98cbc20ff4a1c4a7ac84ab72
charley-mark <charley.grad@gmail.com>
2024-12-05 16:04:35 -0500

    current working main.go in client

--- client/main.go
+++ client/main.go
@@ -18,7 +18,7 @@ import (
 
 func main() {
 	tlsConfig := &tls.Config{InsecureSkipVerify: true}
-	session, err := quic.DialAddr(context.Background(), "132.235.1.37:4242", tlsConfig, nil)
+	session, err := quic.DialAddr(context.Background(), "132.235.1.17:4242", tlsConfig, nil)
 	if err != nil {
 		log.Fatalf("Failed to connect to server: %v", err)
 	}
